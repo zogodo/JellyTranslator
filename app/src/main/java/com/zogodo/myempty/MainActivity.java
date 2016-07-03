26659329c8fbc1e3f0c17fbd9590a172b49ae8f6
周国栋 <742782908@qq.com>
2016-07-03 11:25:00 +0800

    五种按键

--- app/src/main/java/com/zogodo/myempty/MainActivity.java
+++ app/src/main/java/com/zogodo/myempty/MainActivity.java
@@ -5,7 +5,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,15 +15,34 @@ public class MainActivity extends AppCompatActivity {
         findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(getApplicationContext(), "Hello!", Toast.LENGTH_LONG).show();
+                Toast.makeText(getApplicationContext(), "Hello world!", Toast.LENGTH_LONG).show();
             }
         });
 
-        findViewById(R.id.button2).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(getApplicationContext(), "Hello2222222!", Toast.LENGTH_LONG).show();
-            }
-        });
+        findViewById(R.id.button2).setOnClickListener(new HandleClick2());
+
+        findViewById(R.id.button3).setOnClickListener(handleClick3);
+
+        findViewById(R.id.button4).setOnClickListener(this);
+    }
+
+    private class HandleClick2 implements View.OnClickListener{
+        public void onClick(View arg0) {
+            Toast.makeText(getApplicationContext(), "Hello world 2 !", Toast.LENGTH_LONG).show();
+        }
+    }
+
+    private View.OnClickListener handleClick3 = new View.OnClickListener(){
+        public void onClick(View arg0) {
+            Toast.makeText(getApplicationContext(), "Hello world 3 !", Toast.LENGTH_LONG).show();
+        }
+    };
+
+    public void onClick(View arg0) {
+        Toast.makeText(getApplicationContext(), "Hello world 4 !", Toast.LENGTH_LONG).show();
+    }
+
+    public void HandleClick5(View arg0) {
+        Toast.makeText(getApplicationContext(), "Hello world 4 !", Toast.LENGTH_LONG).show();
     }
 }
